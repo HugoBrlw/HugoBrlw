21d6b487cbd5f7b763a59d32ede8bd2ae6f090a4
Hugo Barlow <hugobrlw@gmail.com>
2024-10-07 20:38:34 +0200

    Added Contact page
    Contact.html added and some quality of life changes

---
+++ submit_form.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Do something with the data, like send an email or save to a database
+    // For example, sending an email (make sure your server is configured to send emails)
+    $to = "hugobrlw@.com"; // Change this to your email address
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+} else {
+    echo "Invalid request method.";
+}
+?>
